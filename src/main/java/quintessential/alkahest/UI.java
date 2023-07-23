f274803ba7b9c5c92c894612c8db2e062afbbcc8
l-Luna <izzeldeen03@gmail.com>
2023-07-23 20:27:39 +0100

    implement Installs menu

--- src/main/java/quintessential/alkahest/UI.java
+++ src/main/java/quintessential/alkahest/UI.java
@@ -2,17 +2,18 @@ package quintessential.alkahest;
 
 import com.github.weisj.darklaf.LafManager;
 import com.github.weisj.darklaf.components.OverlayScrollPane;
+import com.github.weisj.darklaf.iconset.IconSet;
 import com.github.weisj.darklaf.theme.OneDarkTheme;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.net.URL;
+import java.nio.file.Path;
 import java.util.List;
 
 public final class UI{
@@ -29,7 +30,7 @@ public final class UI{
 		pane.addTab("dummy", new JPanel());
 		pane.addTab("Get Mods", getModsPanel());
 		pane.addTab("Installed Mods", new JPanel());
-		pane.addTab("Quintessential", new JPanel());
+		pane.addTab("Installs", getInstallsPanel());
 		pane.addTab("Settings", new JPanel());
 		var runButton = new JButton("Run Quintessential"){
 			public void setEnabled(boolean b){}
@@ -44,16 +45,14 @@ public final class UI{
 		pane.setSelectedIndex(1);
 		frame.add(pane);
 		
-		frame.setSize(600, 400);
+		frame.setSize(800, 500);
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 	}
 	
 	private static JComponent getModsPanel(){
-		var listPanel = new JPanel();
-		listPanel.setLayout(new BoxLayout(listPanel, BoxLayout.Y_AXIS));
-		listPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+		JPanel listPanel = createVPanel(0);
 		Repos.fetchAllRepos(1).stream().map(UI::displayRepo).forEach(listPanel::add);
 		listPanel.add(Box.createVerticalGlue());
 		
@@ -61,10 +60,7 @@ public final class UI{
 	}
 	
 	private static JComponent displayRepo(ModRepo repo){
-		JPanel repoPanel = new JPanel();
-		repoPanel.setBorder(new EmptyBorder(8, 8, 8, 8));
-		repoPanel.setLayout(new BoxLayout(repoPanel, BoxLayout.X_AXIS));
-		repoPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+		JPanel repoPanel = createHPanel(8);
 		
 		List<ModVersion> versions = Versions.fetchVersions(repo, 1); // TODO: limit to latest release
 		if(versions.size() == 0){
@@ -101,6 +97,85 @@ public final class UI{
 			return new JLabel(icon);
 		}catch(IOException e){
 			throw new RuntimeException(e);
+		}
+	}
+	
+	private static JComponent getInstallsPanel(){
+		JPanel listPanel = createVPanel(0);
+		Main.installs.stream().map(UI::displayInstall).forEach(listPanel::add);
+		listPanel.add(Box.createVerticalGlue());
+		return new OverlayScrollPane(listPanel);
+	}
+	
+	private static JComponent displayInstall(OmInstall install){
+		JPanel installPanel = createVPanel(8);
+		
+		var pathLabel = new JLabel(install.path().toAbsolutePath().toString());
+		pathLabel.setFont(new Font("Monospaced", Font.PLAIN, 13));
+		installPanel.add(pathLabel);
+		installPanel.add(Box.createRigidArea(new Dimension(8, 8)));
+		
+		var details = createHPanel(0);
+		if(install.hasMods())
+			details.add(new JButton(new RunOmAction(false, install)));
+		else
+			details.add(new JButton("Install Quintessential", IconSet.iconLoader().getIcon("navigation/arrow/thick/arrowDown.svg")));
+		details.add(new JButton(new RunOmAction(true, install)));
+		details.add(new JButton(new OpenFolderAction(install.path())));
+		installPanel.add(details);
+		return installPanel;
+	}
+	
+	private static JPanel createHPanel(int border){
+		JPanel hPanel = new JPanel();
+		hPanel.setBorder(new EmptyBorder(border, border, border, border));
+		hPanel.setLayout(new BoxLayout(hPanel, BoxLayout.X_AXIS));
+		hPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+		return hPanel;
+	}
+	
+	private static JPanel createVPanel(int border){
+		JPanel vPanel = new JPanel();
+		vPanel.setBorder(new EmptyBorder(border, border, border, border));
+		vPanel.setLayout(new BoxLayout(vPanel, BoxLayout.Y_AXIS));
+		vPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+		return vPanel;
+	}
+	
+	private static class RunOmAction extends AbstractAction{
+		
+		private final boolean vanilla;
+		private final OmInstall install;
+		
+		public RunOmAction(boolean vanilla, OmInstall install){
+			super(vanilla ? "Start vanilla" : "Start with Quintessential", IconSet.iconLoader().getIcon("navigation/arrow/thick/arrowRight.svg"));
+			this.vanilla = vanilla;
+			this.install = install;
+		}
+		
+		public void actionPerformed(ActionEvent e){
+			if(vanilla)
+				Installs.startVanilla(install);
+			else
+				Installs.startModded(install);
+		}
+	}
+	
+	private static class OpenFolderAction extends AbstractAction{
+		
+		private final Path path;
+		
+		public OpenFolderAction(Path path){
+			super("Open folder", IconSet.iconLoader().getIcon("files/folder.svg"));
+			this.path = path;
+		}
+		
+		public void actionPerformed(ActionEvent e){
+			try{
+				Desktop.getDesktop().open(path.toFile());
+			}catch(IOException ex){
+				throw new UncheckedIOException(ex);
+			}
 		}
 	}
 }
